33373a14bd17907f22ffa8a03807dc992a0c88ec
Olivia Nordquist <nolivia@umich.edu>
2015-08-17 15:09:30 -1000

    coool beans

--- main.js
+++ main.js
@@ -13,11 +13,19 @@ function update() {
   if (lower_hour === 0)
     lower_hour = 12;
 
+  var minute = now.getMinutes();
+  var lower_opacity = 1 - (minute/60)
+  var upper_opacity = minute/60
+
 
 
   $("#lower").text(hour_string(lower_hour));
   $("#upper").text(hour_string(upper_hour));
 
+  $("#lower").css("opacity", lower_opacity);
+  $("#upper").css("opacity", upper_opacity);
+
 };
 
+setInterval(update, 1000);
 update();
